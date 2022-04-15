f7ea466af3d8953416f1bc185f2203dd0579c5e5
Andrey Krymski <6714223@mail.ru>
2022-04-15 19:06:17 +0300

    gcd

---
+++ bin/brain-gcd.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import nod from '../src/Games/gcd.js';
+
+nod();
---
+++ src/Games/gcd.js
@@ -0,0 +1,35 @@
+import readlineSync from 'readline-sync';
+import youName from '../cli.js';
+import getRandomNumbers from '../getRandomNumber.js';
+
+const nod = () => {
+  const name = youName();
+  let result = 0;
+  console.log('Find the greatest common divisor of given numbers.');
+  for (let i = 0; i < 3; i += 1) {
+    const number1 = getRandomNumbers(51);
+    const number2 = getRandomNumbers(51);
+    console.log(`Question: ${number1} ${number2}`);
+    const answer = readlineSync.question('Your answer: ');
+    const answerNumbers = Number(answer);
+    for (let m = number1; m > 0; m -= 1) {
+      if ((number1 === 0) || (number2 === 0)) {
+        result = 0;
+        m = 0;
+      } else if ((number1 % m === 0) && (number2 % m === 0)) {
+        result = m;
+        m = 0;
+      } else if (m === 1) {
+        result = 1;
+      }
+    }
+    if (result === answerNumbers) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${answer}' is wrong answer. Correct answer was ${result}.`);
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+  }console.log(`Congratulations, ${name}!`);
+};
+export default nod;
